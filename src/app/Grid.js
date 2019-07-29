3f308249487eb33b90fdf2bf2b34e22892ed9a9b
Abiel Villarosa <john_abiel@yahoo.com>
2019-07-28 23:23:10 -0400

    Fixed Grid load issue

--- src/app/Grid.js
+++ src/app/Grid.js
@@ -3,31 +3,53 @@ import APIService from '../api'
 import img2 from './images/2.png';
 import img3 from './images/3.png';
 import img4 from './images/4.png';
+import BlockchainClient from '../blockchain';
 
-const api = new APIService()
+const api = new APIService();
+const blockchain = new BlockchainClient();
 
 class GridBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {result : ''};
     
-        this.handleGridLoad = this.handleGridLoad.bind(this);
+        // this.handleGridLoad = this.handleGridLoad.bind(this);
+        this.handleClick = this.handleClick.bind(this);
       };
     
-    handleGridLoad(e){
+    // handleGridLoad(e){
+    //     e.preventDefault()
+    //     api.search().then( res => {
+    //      return res.json()
+    //     }).then( res => {    
+    //       this.setState({result : res});
+    //       localStorage.setItem('result', JSON.stringify(res));
+    //       console.log(res);
+    //     })
+    // };
+
+    handleClick(e){
         e.preventDefault()
-        api.search().then( res => {
-         return res.json()
-        }).then( res => {    
-          this.setState({result : res});
+        // let swopRefNo = this.state.result.data.swopRefNo
+        let swopRefNo = 'BKN#f16238a1';
+    
+        blockchain.buyTicket(swopRefNo, 15000000).then( res => {
           console.log(res);
+          this.setState({txHash : res});
         })
-    };
+      };
 
     componentWillMount(){
         // Get Booking Details from Local Storage
-        let result = JSON.parse(localStorage.getItem('result'));
-        this.setState({result: result})
+        api.search('').then( res => {
+            return res.json()
+           }).then( res => {    
+             this.setState({result : res});
+             localStorage.setItem('result', JSON.stringify(res));
+             console.log(res);
+           })
+        // let result = JSON.parse(localStorage.getItem('result'));
+        // this.setState({result: result})
     
       }
 
@@ -48,8 +70,10 @@ class GridBuilder extends Component {
                     <figure class="image">
                         <img src={ img2 } style={{width: 300 , height: 150 }}></img>
                     </figure>
-                    <p class="subtitle">{this.state.result.data.departureDateTime}</p>
-                    <p class="subtitle">{this.state.result.data.amount}</p>
+                    <p class="subtitle">Ottawa</p>
+                    <p class="subtitle">09 September 2019 07:00AM</p>
+                    <p class="subtitle">$150</p>
+                    <span><a class="button is-black" style={{width: 150}} onClick={this.handleClick}>Swop</a></span>
                     </article>
                 </div>
                 <div class="tile is-parent">
@@ -57,8 +81,10 @@ class GridBuilder extends Component {
                     <figure class="image">
                         <img src={ img3 } style={{width: 300 , height: 150 }}></img>
                     </figure>
-                    <p class="subtitle"> {this.state.result.data.departureDateTime}</p>
-                    <p class="subtitle">{this.state.result.data.amount}</p>
+                    <p class="subtitle">Calgary</p>
+                    <p class="subtitle">21 September 2019 09:00AM</p>
+                    <p class="subtitle">$250</p>
+                    <span><a class="button is-black" style={{width: 150}} onClick={this.handleClick}>Swop</a></span>
                     </article>
                 </div>
                 <div class="tile is-parent">
@@ -66,8 +92,10 @@ class GridBuilder extends Component {
                     <figure class="image">
                         <img src={ img4 } style={{width: 300 , height: 150 }}></img>
                     </figure>
-                    <p class="subtitle"> {this.state.result.data.departureDateTime}</p>
-                    <p class="subtitle">{this.state.result.data.amount}</p>
+                    <p class="subtitle">Montreal</p>
+                    <p class="subtitle">23 September 2019 08:00PM</p>
+                    <p class="subtitle">$210</p>
+                    <span><a class="button is-black" style={{width: 150}} onClick={this.handleClick}>Swop</a></span>
                     </article>
                 </div>
                 </div>
