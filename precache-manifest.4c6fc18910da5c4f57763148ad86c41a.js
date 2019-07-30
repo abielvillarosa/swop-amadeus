916d7863acdb953d692a274b97c568585feea795
Abiel Villarosa <john_abiel@yahoo.com>
2019-07-30 19:40:05 -0400

    Updates

---
+++ precache-manifest.4c6fc18910da5c4f57763148ad86c41a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6698c8ca6645dda1feec46e8c25609",
+    "url": "/swop-amadeus/index.html"
+  },
+  {
+    "revision": "2af6b739a236896e599d",
+    "url": "/swop-amadeus/static/css/main.18b8860d.chunk.css"
+  },
+  {
+    "revision": "986ac3f08f42bdd1b80a",
+    "url": "/swop-amadeus/static/js/2.7546d86c.chunk.js"
+  },
+  {
+    "revision": "2af6b739a236896e599d",
+    "url": "/swop-amadeus/static/js/main.207129db.chunk.js"
+  },
+  {
+    "revision": "94e7d77779164c40b437",
+    "url": "/swop-amadeus/static/js/runtime~main.4eebfd3a.js"
+  },
+  {
+    "revision": "3328e0b74fd1bd1eacf15f491dff9491",
+    "url": "/swop-amadeus/static/media/2.3328e0b7.png"
+  },
+  {
+    "revision": "d35c7bf9bd9807309b1edd6fdf3cd1da",
+    "url": "/swop-amadeus/static/media/3.d35c7bf9.png"
+  },
+  {
+    "revision": "4196c7887902aee4f6c10f04b69777c4",
+    "url": "/swop-amadeus/static/media/4.4196c788.png"
+  },
+  {
+    "revision": "413abe0d020f52e86b2ea338a92a5f5f",
+    "url": "/swop-amadeus/static/media/Logo.413abe0d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swop-amadeus/swop/precache-manifest.44ca080eafdd47f48ebba3a2df157cde.js"
+  "/swop-amadeus/precache-manifest.4c6fc18910da5c4f57763148ad86c41a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swop-amadeus/swop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swop-amadeus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
